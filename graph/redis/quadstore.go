a9d45a0d83134e0a57b21300c69f42617dcbf84b
Mario Castro <mariocaster@gmail.com>
2016-02-12 23:54:00 +0100

    Test makeTestStore written and License added

--- graph/redis/quadstore.go
+++ graph/redis/quadstore.go
@@ -1,1 +1,55 @@
 package redis
+
+import (
+	"time"
+
+	"github.com/google/cayley/graph/memstore/b"
+	"github.com/sayden/cayley/graph"
+	"github.com/sayden/cayley/quad"
+)
+
+type QuadStore struct {
+	nextID     int64
+	nextQuadID int64
+	idMap      map[string]int64
+	revIDMap   map[int64]string
+	log        []LogEntry
+	size       int64
+	index      QuadDirectionIndex
+	// vip_index map[string]map[int64]map[string]map[int64]*b.Tree
+}
+
+type QuadDirectionIndex struct {
+	index [4]map[int64]*b.Tree
+}
+
+type LogEntry struct {
+	ID        int64
+	Quad      quad.Quad
+	Action    graph.Procedure
+	Timestamp time.Time
+	DeletedBy int64
+}
+
+func NewQuadDirectionIndex() QuadDirectionIndex {
+	return QuadDirectionIndex{[...]map[int64]*b.Tree{
+		quad.Subject - 1:   make(map[int64]*b.Tree),
+		quad.Predicate - 1: make(map[int64]*b.Tree),
+		quad.Object - 1:    make(map[int64]*b.Tree),
+		quad.Label - 1:     make(map[int64]*b.Tree),
+	}}
+}
+
+func newQuadStore() *QuadStore {
+	return &QuadStore{
+		idMap:    make(map[string]int64),
+		revIDMap: make(map[int64]string),
+
+		// Sentinel null entry so indices start at 1
+		log: make([]LogEntry, 1, 200),
+
+		index:      NewQuadDirectionIndex(),
+		nextID:     1,
+		nextQuadID: 1,
+	}
+}
--- graph/redis/redis_test.go
+++ graph/redis/redis_test.go
@@ -1,1 +1,75 @@
+// Copyright 2014 The Cayley Authors. All rights reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
 package redis
+
+import (
+	"github.com/google/cayley/quad"
+	"github.com/google/cayley/writer"
+	"github.com/sayden/cayley/graph"
+)
+
+// This is a simple test graph.
+//
+//    +---+                        +---+
+//    | A |-------               ->| F |<--
+//    +---+       \------>+---+-/  +---+   \--+---+
+//                 ------>|#B#|      |        | E |
+//    +---+-------/      >+---+      |        +---+
+//    | C |             /            v
+//    +---+           -/           +---+
+//      ----    +---+/             |#G#|
+//          \-->|#D#|------------->+---+
+//              +---+
+//
+var simpleGraph = []quad.Quad{
+	{"A", "follows", "B", ""},
+	{"C", "follows", "B", ""},
+	{"C", "follows", "D", ""},
+	{"D", "follows", "B", ""},
+	{"B", "follows", "F", ""},
+	{"F", "follows", "G", ""},
+	{"D", "follows", "G", ""},
+	{"E", "follows", "F", ""},
+	{"B", "status", "cool", "status_graph"},
+	{"D", "status", "cool", "status_graph"},
+	{"G", "status", "cool", "status_graph"},
+}
+
+func makeTestStore(data []quad.Quad) (*QuadStore, graph.QuadWriter, []pair) {
+	seen := make(map[string]struct{})
+	qs := newQuadStore()
+	var (
+		val int64
+		ind []pair
+	)
+	writer, _ := writer.NewSingleReplication(qs, nil)
+	for _, t := range data {
+		for _, qp := range []string{t.Subject, t.Predicate, t.Object, t.Label} {
+			if _, ok := seen[qp]; !ok && qp != "" {
+				val++
+				ind = append(ind, pair{qp, val})
+				seen[qp] = struct{}{}
+			}
+		}
+
+		writer.AddQuad(t)
+	}
+	return qs, writer, ind
+}
+
+type pair struct {
+	query string
+	value int64
+}
